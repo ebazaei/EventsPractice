ce3cf69d64490571fd0a948f06f56d74c08c1a98
Your Name <youremail@domain.com>
2023-08-06 13:58:52 +0200

    add-remove

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
-//Event Listeners / Multi 
-let btn3 = document.getElementById('btn3');
-let box3 = document.getElementById('box3');
 
-function EventListener1(){
-    document.getElementById('box3-2').innerHTML = 'test bbbbb';
-}
-function EventListener2(){
-    alert('test');
-}
-function EventListener3(){
-    box3.style.backgroundColor = '#ff0000';
-}
-function EventListener4(){
-    box3.style.backgroundColor = '#ffffff';
-}
-btn3.addEventListener('click', EventListener1);
-btn3.addEventListener('click', EventListener2);
-btn3.addEventListener('mouseover', EventListener3);
-btn3.addEventListener('mouseout', EventListener4);
-
-
-// //Event test 2
-// let btn2 = document.getElementById('btn2');
-// let box2 = document.getElementById('box2');
-// btn2.onclick = function(){
-//     box2.innerHTML = 'test2';
-// }
 
 // //Event onclick
 // var btn1 = document.getElementById('btn1');
 // btn1.onclick = function(){
 //     document.getElementById('place1').innerHTML = Date();
 // }
+
 
 // //Event onchange
 
@@ -40,6 +14,49 @@ btn3.addEventListener('mouseout', EventListener4);
 //   x.value = x.value.toUpperCase();
 // }
 
+// //Event test 2
+// let btn2 = document.getElementById('btn2');
+// let box2 = document.getElementById('box2');
+// btn2.onclick = function(){
+//     box2.innerHTML = 'test2';
+// }
 
+
+
+// //Event Listeners 1 / Multi 
+// let btn3 = document.getElementById('btn3');
+// let box3 = document.getElementById('box3');
+
+// function EventListener1(){
+//     document.getElementById('box3-2').innerHTML = 'test bbbbb';
+// }
+// function EventListener2(){
+//     alert('test');
+// }
+// function EventListener3(){
+//     box3.style.backgroundColor = '#ff0000';
+// }
+// function EventListener4(){
+//     box3.style.backgroundColor = '#ffffff';
+// }
+// btn3.addEventListener('click', EventListener1);
+// btn3.addEventListener('click', EventListener2);
+// btn3.addEventListener('mouseover', EventListener3);
+// btn3.addEventListener('mouseout', EventListener4);
+
+
+
+
+//Event Listeners 2 / Add-Remove 
+
+let btn4 = document.getElementById('btn4');
+let box4 = document.getElementById('box4');
+
+function addEventL(){
+    alert('Hi');
+    btn4.removeEventListener('click', addEventL);
+}
+
+btn4.addEventListener('click', addEventL);
 
 
