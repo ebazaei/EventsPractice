cb62a89d749a7af5acf5cf307c82b5cb3d7893b7
Your Name <youremail@domain.com>
2023-08-06 12:39:37 +0200

    event 2 basic

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-var body = document.getElementsByTagName('body')[0];
-body.style.backgroundColor = '#ccc';
-
-//Event onclick
-var btn1 = document.getElementById('btn1');
-btn1.onclick = function(){
-    document.getElementById('place1').innerHTML = Date();
+//Event test 2
+let btn2 = document.getElementById('btn2');
+let box2 = document.getElementById('box2');
+btn2.onclick = function(){
+    box2.innerHTML = 'test2';
 }
 
-//Event onchange
+// //Event onclick
+// var btn1 = document.getElementById('btn1');
+// btn1.onclick = function(){
+//     document.getElementById('place1').innerHTML = Date();
+// }
 
-function myFunction() {
-  var x = document.getElementById("fname");
-  x.value = x.value.toUpperCase();
-}
+// //Event onchange
+
+// function myFunction() {
+//   var x = document.getElementById("fname");
+//   x.value = x.value.toUpperCase();
+// }
+
+//Event Listeners
+
 
